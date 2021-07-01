e6d8e928bd524a6beaab49e93ba9aa619093017a
Hungdepzaj <86783728+Hungdepzaj@users.noreply.github.com>
2021-07-01 20:37:44 +0700

    Update config.js

--- config.js
+++ config.js
@@ -14,6 +14,6 @@ const CONFIG = {
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
     messDesc: 'Tối nay 12h anh qua đón pay lắc em nhé.',
-    btnAccept: 'Okiiiii lun <3',
+    btnAccept: 'Okiiiii lun <33',
     messLink: 'https://www.facebook.com/profile.php?id=100041467350723
 }
